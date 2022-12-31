b3c90571a179389b7e959f8e8458809841847d77
Eric Stampa <eric.stampa@t-online.de>
2022-12-31 15:07:21 +0100

    build: added manifest and meta links for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,28 @@ export default defineConfig({
 			provider: "c8",
 		},
 	},
-	plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Clip Systems App",
+				short_name: "Clip App",
+				description: "Clip Systems calculation App",
+				theme_color: "#e11d48",
+				icons: [
+					{
+						src: "/180.png",
+						sizes: "180x180",
+						type: "image/png",
+					},
+					{
+						src: "/1024.png",
+						sizes: "1024x1024",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
 });
