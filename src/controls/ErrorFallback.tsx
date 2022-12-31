6d9591c3cfbf504601c3f8fcb794134f56bf9281
Eric Stampa <eric.stampa@t-online.de>
2022-12-31 14:53:26 +0100

    feat: added error boundary to catch application errors

---
+++ src/controls/ErrorFallback.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import Button from "./Layout/Button";
+import Container from "./Layout/Container";
+import HelperText from "./Layout/HelperText";
+
+interface ErrorFallbackProps {
+	error: Error;
+	resetErrorBoundary: () => void;
+}
+
+const ErrorFallback: FC<ErrorFallbackProps> = ({ error, resetErrorBoundary }) => {
+	return (
+		<div className="w-screen h-screen grid place-items-center" role="alert">
+			<Container>
+				<span>Ein Fehler ist aufgetretten</span>
+				<HelperText type="error">{error.message}</HelperText>
+				<Button onClick={resetErrorBoundary}>Erneut Versuchen</Button>
+			</Container>
+		</div>
+	);
+};
+
+export default ErrorFallback;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import Router from "./pages/Router";
 import { RecoilRoot } from "recoil";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./controls/ErrorFallback";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<StrictMode>
-		<RecoilRoot>
-			<Router />
-		</RecoilRoot>
+		<ErrorBoundary FallbackComponent={ErrorFallback}>
+			<RecoilRoot>
+				<Router />
+			</RecoilRoot>
+		</ErrorBoundary>
 	</StrictMode>,
 );
