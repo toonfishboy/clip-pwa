6b22ff3a552385103100dbb298333c52f2a277a0
ToonVish <eric.stampa@t-online.de>
2024-04-11 20:30:26 +0200

    test: add new useUdpateValue test

---
+++ src/hooks/useUpdateValue.test.ts
@@ -0,0 +1,26 @@
+import { act, renderHook } from '@testing-library/react';
+import { describe, expect, jest, test } from 'bun:test';
+import { useState } from 'react';
+import { useUpdateValue } from './useUpdateValue';
+
+describe('values are updated with the key', () => {
+	test('should update the value of the key', () => {
+		const updateValue = jest.fn();
+		const { result } = renderHook(() => {
+			const [value, setValue] = useState({ key: 'value' });
+
+			return useUpdateValue(value, (event) => {
+				updateValue();
+				setValue(event);
+			});
+		});
+
+		expect(result.current[0]('key')).toBe('value');
+		expect(updateValue).toHaveBeenCalledTimes(0);
+		act(() => {
+			result.current[1]('key')('new value');
+		});
+		expect(result.current[0]('key')).toBe('new value');
+		expect(updateValue).toHaveBeenCalledTimes(1);
+	});
+});
